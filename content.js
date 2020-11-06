c4a7cbb217913d5ac5ccd48fcf540543b83c4a92
meng.shan <meng.shan@hirede.com>
2020-11-06 11:44:59 +0800

    first

---
+++ content.js
@@ -0,0 +1,9 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+if(location.host == 'www.cnswift.org') {
+  let sideBar = document.getElementById("sidebar");
+  if(sideBar) {
+    sideBar.style.backgroundColor = '#666';
+  }
+}
